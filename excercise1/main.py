008048b79bf4c20370cc72f37f0a2e806b5c6b93
Jonathan Keinan <jonathan.k@qspark.co>
2019-11-16 23:26:43 +0200

    * basic program

--- excercise1/main.py
+++ excercise1/main.py
@@ -1,1 +1,27 @@
 import nltk
+import urllib.request
+import argparse
+import logging
+
+
+logging.basicConfig()
+logger = logging.getLogger('exc1')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url',
+                        default='http://www.gutenberg.org/files/84/84-0.txt',
+                        help='Where to download the text from')
+    args = parser.parse_args()
+    page = urllib.request.urlopen(args.url)
+    content = page.read()
+    logger.warning(content)
+
+
+if __name__ == '__main__':
+    try:
+        logger.debug('Hello')
+        main()
+    finally:
+        logger.debug('Bye bye...')
