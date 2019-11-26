cb38378ebe20366f59d98924b7987b008321dd1e
Jonathan Keinan <jonathan.k@qspark.co>
2019-11-26 21:10:21 +0200

    * mostly working chapter removal function

--- excercise1/main.py
+++ excercise1/main.py
@@ -25,7 +25,10 @@ def remove_header_and_footer(lines, start_barrier='*** START', end_barrier='*** 
 
 
 # http://www.gutenberg.org/files/60773/60773-0.txt
-def find_chapter_words(lines, minimal_length=4, appearance_threshold=5):
+def find_chapter_words(lines,
+                       minimal_word_length=4,
+                       appearance_threshold=2,
+                       words_in_sentance=4):
     class WordInfo:
         def __init__(self):
             self.count = 0
@@ -34,15 +37,24 @@ def find_chapter_words(lines, minimal_length=4, appearance_threshold=5):
         def __repr__(self):
             return repr(self.positions)
     word_to_info = {}
+    chapter_punctuation = string.punctuation
     for line_number, line in enumerate(lines):
         words = line.split()
-        if line.startswith('***'):
-            print(words)
-        if not words or len(words) == 0:
+        if not words or len(words) < 1:
             continue
-        first_word = words[0]
+        # First word should look like Chapter or CHAPTER or chapter.
+        # We want to avoid connecting words such as OF and THE
+        first_word = words[0].strip(chapter_punctuation)
+        if len(first_word) < minimal_word_length:
+            continue
+        # last word should be the chapter id, e.g: 1, 11, X, I, A
+        # so it is either a number or a capital text
+        last_word = words[-1].strip(chapter_punctuation)
         # Should mostly be in the format of "Chapter _" so we expect at most two words
-        if len(words) > 2 or len(first_word) < minimal_length:
+        is_last_word_chapter_index = last_word.isdigit() or last_word.isupper()
+        is_chapter_candidate = first_word.isalpha() and is_last_word_chapter_index
+        is_sentance = len(words) >= words_in_sentance
+        if not is_chapter_candidate or is_sentance:
             continue
         if first_word not in word_to_info:
             word_to_info[first_word] = WordInfo()
@@ -50,8 +62,8 @@ def find_chapter_words(lines, minimal_length=4, appearance_threshold=5):
         info.count += 1
         info.positions.append(line_number)
     meta_content_words = {k:v for (k,v) in word_to_info.items()
-                          if v.count > appearance_threshold
-                          and (k.istitle() or k.isupper() or k == barrier_word)}
+                          if v.count >= appearance_threshold
+                          and (k.istitle() or k.isupper())}
     return meta_content_words.keys()
 
 
@@ -64,25 +76,28 @@ def remove_chapters(lines, chapter_expected_apperances=2):
         def __repr__(self):
             return repr(self.positions)
     chapter_lines = {}
-    chapter_words = find_chapter_words(lines, appearance_threshold=4)
+    chapter_words = find_chapter_words(lines)
     print('Chapter Words: {}'.format(chapter_words))
     for line_number, line in enumerate(lines):
+        stripped_line = line.strip()
         for word in chapter_words:
-            if line.startswith(word):
+            if stripped_line.startswith(word):
+
                 if line not in chapter_lines:
-                    chapter_lines[line] = LineInfo()
-                chapter_line = chapter_lines[line]
+                    chapter_lines[stripped_line] = LineInfo()
+                chapter_line = chapter_lines[stripped_line]
                 chapter_line.count += 1
                 chapter_line.positions.append(line_number)
-    print('Chapters: {}'.format(chapter_lines))
+                break
     chapter_lines = {k: v for (k,v) in chapter_lines.items() if v.count == chapter_expected_apperances}
-
     largest_apperances = []
     for line_info in chapter_lines.values():
+        # we iterate line by line so positions are already sorted
         largest_position = line_info.positions[1]
         largest_apperances.append(largest_position)
     # sorting in reverse so we can remove lines without affecting next line deletion
     largest_apperances = sorted(largest_apperances, reverse=True)
+    print(largest_apperances)
     first_chapter_second_apperance = largest_apperances[-1]
     last_chapter_second_apperance = largest_apperances[0]
     lines = lines[:last_chapter_second_apperance]
@@ -103,7 +118,6 @@ def cleanup_text(text):
     # Move to lowercase
     lines = text.splitlines()
     lines = remove_header_and_footer(lines)
-    print('Removed header and footer: {}'.format(lines))
     lines = remove_chapters(lines)
     text_in_lowercase = ''.join(lines)
 
