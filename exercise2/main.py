5041de1e1ad0af1e4303ac29d1a28c094cfcdfd1
Jonathan Keinan <jonathan.k@qspark.co>
2019-12-14 19:52:01 +0200

    * runtime initial fixes

--- exercise2/main.py
+++ exercise2/main.py
@@ -10,6 +10,7 @@ import zipfile
 from collections import Counter
 from math import log2
 from nltk.lm import MLE
+from nltk.lm import Vocabulary
 from nltk.lm.preprocessing import padded_everygram_pipeline
 
 logging.basicConfig(level=logging.INFO)
@@ -26,7 +27,7 @@ def cleanup_text(text, keep_non_english_letters=False):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-w', '--word-list-url',
-                        default='www2.mta.ac.il/~gideon/courses/nlp/data/word_list_20k.txt')
+                        default='http://www2.mta.ac.il/~gideon/courses/nlp/data/word_list_20k.txt')
     parser.add_argument('-c', '--corpora-zip-url',
                         default='http://www2.mta.ac.il/~gideon/courses/nlp/data/corpus_combined.zip')
     parser.add_argument('-t', '--training-corpus',
@@ -34,7 +35,7 @@ def main():
     parser.add_argument('-e', '--testing-corpus',
                         default='persuasion.txt')
     args = parser.parse_args()
-    word_list = requests.get(args.word_list_url).split('\n')
+    word_list = requests.get(args.word_list_url).content.decode('utf-8').split('\n')
     if not os.path.exists(args.training_corpus) or not os.path.exists(args.testing_corpus):
         with requests.get(args.corpora_zip_url, stream=True) as response:
             z = zipfile.ZipFile(io.BytesIO(response.content))
@@ -50,7 +51,7 @@ def main():
         for ngram in range(2):
             train, _ = padded_everygram_pipeline(ngram, clean_training)
             _, vocab = padded_everygram_pipeline(ngram, word_list)
-            model = language_model_type(vocabulary=vocab, order=ngram)
+            model = language_model_type(vocabulary=Vocabulary(vocab), order=ngram)
             cross_entropy = model.entropy(clean_testing)
             logger.info('Cross Entropy for N={}: {}'.format(ngram, cross_entropy))
 
